416a2fbdbe768f99e563dda4a22b2c3f63563b91
catalamarti <marticatalasabate@gmail.com>
2023-06-16 11:30:43 +0100

    add shiny

---
+++ app.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(DataQualityDashboard)
+
+# path to the jsonfile
+jsonPath <- here::here("cdm_gold_202207_dqd.json")
+
+server <- function(input, output, session) {
+  observe({
+    results <- convertJsonResultsFileCase(jsonPath, writeToFile = FALSE, targetCase = "camel")
+    results <- jsonlite::parse_json(jsonlite::toJSON(results))
+    session$sendCustomMessage("results", results)
+  })
+}
+
+ui <- fluidPage(
+  suppressDependencies("bootstrap"),
+  shiny::htmlTemplate(filename = here::here("www", "index.html")),
+  tags$head(
+    tags$script(src = "js/loadResults.js"),
+    tags$script("Shiny.addCustomMessageHandler('results', loadResults);")
+  )
+)
+
+shiny::shinyApp(ui = ui, server = server)
+
